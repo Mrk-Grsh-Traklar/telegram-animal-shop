9e7d41237fdda77c9a81f79ebcbc9089b32f143e
Mrk-Grsh-Traklar <i29343229@gmail.com>
2024-10-16 20:50:45 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -12,10 +12,9 @@ print('started')
 def mein_menu(message):
     markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
     profile = types.KeyboardButton('profile')
-    help = types.KeyboardButton('help')
     support = types.KeyboardButton('support')
-    donate = types.KeyboardButton('donate')
-    markup.add(profile, help, support,donate)
+    buy = types.KeyboardButton('buy')
+    markup.add(profile, support, buy )
     bot.send_message(message.chat.id, f"Hello! {message.from_user.first_name}", reply_markup=markup)
 
 # тех.поддержка
@@ -23,7 +22,29 @@ def mein_menu(message):
 def support(message):
     bot.send_message(message.chat.id, f"Hello! if you encounter difficulties contact technical support \n @assmaser")
 
+# открытие листа покупок 
+# 1) выбор животного
+# 2) выбор категории товара
+# 3) выбор товара
+@bot.message_handler(regexp='buy')
+def animal(message):
+    markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
+    cat = types.KeyboardButton('cat')
+    dog = types.KeyboardButton('dog')
+    chinchilla = types.KeyboardButton('chinchilla')
+    parrot = types.KeyboardButton('parrot')
+    snake = types.KeyboardButton('snake')
+    lizard =  types.KeyboardButton('lizard')
+    main = types.KeyboardButton('main')
+    markup.add(cat,dog,chinchilla,parrot,snake,lizard,main)
+    bot.send_message(message.chat.id, f'select animal category:', reply_markup=markup)
 
+# катигории товара
+@bot.message_handler(regexp='product_category')
+def product_category(message):
+    markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
+
+    markup.add()    
 
 
 
